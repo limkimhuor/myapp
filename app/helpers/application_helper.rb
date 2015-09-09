c0378e578a7d2f97db7ed0dcf917244576275fce
Lim Kimhuor <kimhuorlim@gmail.com>
2015-09-09 09:13:24 +0700

    Add a static pages controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def flash_class level
+    case level
+    when :notice then "alert-info"
+    when :error then "alert-error"
+    when :alert then "alert-warning"
+    when :success then "alert-success"
+    end
+  end
+
+  def full_title pages_title = ""
+    base_title = t "titles.header"
+    if pages_title.empty?
+      base_title
+    else
+      "#{pages_title} | #{base_title}"
+    end
+  end
+
+  def flash_message flash_type
+    t "flash.messages.#{flash_type}", model_name: controller_name.classify
+
+  end
 end
